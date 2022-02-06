6d9437cb3704aa1093177628f670843f3609560c
Manoel Roberto <manoelrobertoms@gmail.com>
2022-02-05 23:50:32 -0300

    Correção das rotas de like e de delete

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ app.delete('/repositories/:id', (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id)
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: 'Repository not found' })
   }
 
@@ -74,9 +74,9 @@ app.post('/repositories/:id/like', (request, response) => {
     return response.status(404).json({ error: 'Repository not found' })
   }
 
-  const likes = ++repositories[repositoryIndex].likes
+  repositories[repositoryIndex].likes = ++repositories[repositoryIndex].likes
 
-  return response.json('likes')
+  return response.json(repositories[repositoryIndex])
 })
 
 module.exports = app
